55d63b9ae96e4fd12bc5b3704af5fc7dffd2cf6f
Jason-Donmoyer <j.donmoyer523@gmail.com>
2021-09-07 17:53:05 -0400

    JS: --refactored event listener anonymous func to function declaration --added Boolean value to click event to toggle light and dark modes

--- js/main.js
+++ js/main.js
@@ -1,71 +1,5 @@
 
 // VARIABLES
-const usernameInput = document.getElementById('username-input');
-const submitBtn = document.querySelector('#submit-btn');
-
-let userAvatar = document.getElementById('user-avatar');
-let githubUsername = document.getElementById('github-username');
-let githubLogin = document.getElementById('github-login');
-let dateJoined = document.getElementById('github-date-joined');
-
-let userRepos = document.getElementById('user-repos');
-let userFollowers = document.getElementById('user-followers');
-let userFollowing = document.getElementById('user-following');
-
-let userLocation = document.getElementById('user-location');
-let userBlog = document.getElementById('user-blog');
-let userTwitterName = document.getElementById('user-twitter-name');
-let userCompany = document.getElementById('user-company');
-
-let locationIcon = document.getElementById('icon-location');
-let locationIconNotAvailable = document.getElementById('icon-location-not-available');
-let blogIcon = document.getElementById('icon-blog');
-let blogIconNotAvailable = document.getElementById('icon-blog-not-available');
-let twitterIcon = document.getElementById('icon-twitter');
-let twitterIconNotAvailable = document.getElementById('icon-twitter-not-available');
-let companyIcon = document.getElementById('icon-company');
-let companyIconNotAvailable = document.getElementById('icon-company-not-available');
-
-// Mode Toggle Variables
-const modeToggleBtn = document.getElementById('mode-toggle-btn');
-const body = document.querySelector('body');
-const logo = document.getElementById('logo');
-const lightHeaderCopy = document.getElementById('light-header-copy');
-const darkHeaderCopy = document.getElementById('dark-header-copy');
-const sunIcon = document.getElementById('icon-sun');
-const moonIcon = document.getElementById('icon-moon');
-const searchBar = document.getElementById('search-bar-container');
-const mainContent = document.getElementById('main-content');
-const githubJoinedCopy = document.getElementById('github-joined-copy');
-const bioCopy = document.getElementById('bio-copy');
-const userStatsContainer = document.getElementById('user-stats');
-const userStats = document.getElementsByClassName('user-stat');
-const userStatNums = document.getElementsByClassName('user-stat-numbers');
-
-
-// Mode Toggle Logic
-modeToggleBtn.addEventListener('click', () => {
-  body.classList.toggle('dark-bg');
-  logo.classList.toggle('light-text-color');
-  lightHeaderCopy.classList.toggle('header-display-none');
-  darkHeaderCopy.classList.toggle('header-display-none');
-  sunIcon.classList.toggle('header-display-none');
-  moonIcon.classList.toggle('header-display-none');
-  searchBar.classList.toggle('dark-blue-grey-bg');
-  usernameInput.classList.toggle('light-text-color');
-  mainContent.classList.toggle('dark-blue-grey-bg');
-  githubUsername.classList.toggle('light-text-color');
-  githubJoinedCopy.classList.toggle('light-text-color');
-  bioCopy.classList.toggle('light-text-color');
-  userStatsContainer.classList.toggle('dark-bg')
-  
-  for (let i = 0; i < userStats.length; i++) {
-    userStats[i].classList.toggle('light-text-color');
-    userStatNums[i].classList.toggle('light-text-color');
-  }
-
-});
-
 
 // Object to store user data
 let newUser = {
@@ -82,6 +16,132 @@ let newUser = {
   company: '',
 }
 
+// DOM Variables
+const usernameInput = document.getElementById('username-input');
+const submitBtn = document.querySelector('#submit-btn');
+
+const userAvatar = document.getElementById('user-avatar');
+const githubUsername = document.getElementById('github-username');
+const githubLogin = document.getElementById('github-login');
+const dateJoined = document.getElementById('github-date-joined');
+
+const userRepos = document.getElementById('user-repos');
+const userFollowers = document.getElementById('user-followers');
+const userFollowing = document.getElementById('user-following');
+
+const userLocation = document.getElementById('user-location');
+const userBlog = document.getElementById('user-blog');
+const userTwitterName = document.getElementById('user-twitter-name');
+const userCompany = document.getElementById('user-company');
+
+const locationIcon = document.getElementById('icon-location');
+const locationIconNotAvailable = document.getElementById('icon-location-not-available');
+const blogIcon = document.getElementById('icon-blog');
+const blogIconNotAvailable = document.getElementById('icon-blog-not-available');
+const twitterIcon = document.getElementById('icon-twitter');
+const twitterIconNotAvailable = document.getElementById('icon-twitter-not-available');
+const companyIcon = document.getElementById('icon-company');
+const darkModeCompanyIcon = document.getElementById('dm-icon-company');
+const companyIconNotAvailable = document.getElementById('icon-company-not-available');
+
+// Mode Toggle Variables
+const modeToggleBtn = document.getElementById('mode-toggle-btn');
+const body = document.querySelector('body');
+const logo = document.getElementById('logo');
+const lightHeaderCopy = document.getElementById('light-header-copy');
+const darkHeaderCopy = document.getElementById('dark-header-copy');
+const sunIcon = document.getElementById('icon-sun');
+const moonIcon = document.getElementById('icon-moon');
+const searchBar = document.getElementById('search-bar-container');
+const mainContent = document.getElementById('main-content');
+const githubJoinedCopy = document.getElementById('github-joined-copy');
+const bioCopy = document.getElementById('bio-copy');
+const userStatsContainer = document.getElementById('user-stats');
+const userStats = document.getElementsByClassName('user-stat');
+const userStatNums = document.getElementsByClassName('user-stat-numbers');
+const userSocialInfo = document.getElementsByClassName('user-social-info');
+
+let darkMode = false;
+
+// Mode Toggle Logic
+modeToggleBtn.addEventListener('click', () => {
+  if (darkMode === false) {
+    darkMode = true;
+  } else if (darkMode === true) {
+    darkMode = false;
+  }
+
+  toggleModes();
+
+  
+  
+
+});
+
+function toggleModes() {
+  if (darkMode === true) {
+    body.classList.add('dark-bg');
+    logo.classList.add('light-text-color');
+    lightHeaderCopy.classList.remove('header-display-none');
+    darkHeaderCopy.classList.add('header-display-none');
+    sunIcon.classList.remove('header-display-none');
+    moonIcon.classList.add('header-display-none');
+    searchBar.classList.add('dark-blue-grey-bg');
+    usernameInput.classList.add('light-text-color');
+    mainContent.classList.add('dark-blue-grey-bg');
+    githubUsername.classList.add('light-text-color');
+    githubJoinedCopy.classList.add('light-text-color');
+    bioCopy.classList.add('light-text-color');
+    userStatsContainer.classList.add('dark-bg')
+  
+    for (let i = 0; i < userStats.length; i++) {
+      userStats[i].classList.add('light-text-color');
+      userStatNums[i].classList.add('light-text-color');
+    }
+
+    for (let i = 0; i < userSocialInfo.length; i++) {
+      if (!userSocialInfo[i].classList.contains('not-available')) {
+        userSocialInfo[i].classList.add('light-text-color');
+      }
+    }
+
+    // if (!companyIconNotAvailable) {
+    //   companyIcon.classList.add('header-display-none');
+    //   darkModeCompanyIcon.classList.remove('header-display-none');
+    // }
+
+  } else if (darkMode === false) {
+    body.classList.remove('dark-bg');
+    logo.classList.remove('light-text-color');
+    lightHeaderCopy.classList.add('header-display-none');
+    darkHeaderCopy.classList.remove('header-display-none');
+    sunIcon.classList.add('header-display-none');
+    moonIcon.classList.remove('header-display-none');
+    searchBar.classList.remove('dark-blue-grey-bg');
+    usernameInput.classList.remove('light-text-color');
+    mainContent.classList.remove('dark-blue-grey-bg');
+    githubUsername.classList.remove('light-text-color');
+    githubJoinedCopy.classList.remove('light-text-color');
+    bioCopy.classList.remove('light-text-color');
+    userStatsContainer.classList.remove('dark-bg')
+  
+    for (let i = 0; i < userStats.length; i++) {
+      userStats[i].classList.remove('light-text-color');
+      userStatNums[i].classList.remove('light-text-color');
+    }
+
+    for (let i = 0; i < userSocialInfo.length; i++) {
+      if (!userSocialInfo[i].classList.contains('not-available')) {
+        userSocialInfo[i].classList.remove('light-text-color');
+      }
+    }
+
+
+
+  }
+}
+
+
 // On Page Load
 fetchUser('octocat').then(userData => {
   getUser(userData);
@@ -94,15 +154,6 @@ submitBtn.addEventListener('click', () => {
     getUser(userData);
   });
 });
-
-
-// fetchUser('Jason-Donmoyer').then(userData => {
-//   newUser.login = userData["login"];
-//   newUser.id = userData["id"];
-//   newUser.bio = userData["bio"];
-//   newUser.blog = userData["blog"];
-//   newUser.followers = userData["followers"];
-// });
 
 
 // FUNCTIONS
